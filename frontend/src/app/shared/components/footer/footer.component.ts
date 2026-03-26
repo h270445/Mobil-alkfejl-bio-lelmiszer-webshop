caf52b0e21ffed3b6d7e5badb18bd8e9fcec9f18
h270445 <h270445@stud.u-szeged.hu>
2026-03-26 19:39:46 +0100

    feat(footer): update social media link from Twitter to X and add X icon

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -33,7 +33,7 @@ export class FooterComponent {
   socialLinks = [
     { name: 'Facebook', icon: 'assets/images/facebook-icon.svg', url: 'https://facebook.com' },
     { name: 'Instagram', icon: 'assets/images/instagram-icon.svg', url: 'https://instagram.com' },
-    { name: 'Twitter', icon: 'assets/images/twitter-icon.svg', url: 'https://twitter.com' }
+    { name: 'X', icon: 'assets/images/x-icon.svg', url: 'https://x.com' }
   ];
 
   whyChooseItems = [
